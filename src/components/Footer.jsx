28d87aa57256fbd7007807411c47b5b2a64ab304
Anthony Magana <anthonymagana0705@gmail.com>
2021-11-23 18:14:48 -0800

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Container from '@material-ui/core/Container';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Divider, Link } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import GithubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 
 export default function Footer() {
     return (
-        <AppBar position="static" color="default" style={{marginTop: '80px', paddingTop: '5px', paddingBottom: '5px'}}>
-          <Container maxWidth="md">
-            <Toolbar disableGutters>
-              <Typography variant="body1" color="inherit">
-                © 2021 Anthony Magana
-              </Typography>
-            </Toolbar>
-          </Container>
-        </AppBar>
+      <Container style={{marginTop: '80px'}} maxWidth="md">
+        <Divider  />
+        <div style={{paddingBottom: '5px', marginTop: '35px', textAlign: 'center'}}>
+          <Typography variant="body2" color="inherit" style={{paddingBottom: '15px'}}>
+            This website is <Link style={{color: "#1a85ff"}} href="https://github.com/anthony-magana/Portfolio" rel="noopener noreferrer" target="_blank" >open source</Link>, 
+            and was made with <Link style={{color: "#1a85ff"}} href="https://www.gatsbyjs.com" rel="noopener noreferrer" target="_blank" >Gatsbyjs</Link> and
+            <Link style={{color: "#1a85ff"}} href="https://material-ui.com/" target="_blank" > Material-UI</Link>
+          </Typography>
+          <IconButton> 
+            <Link color="inherit" href="https://github.com/anthony-magana" rel="noopener noreferrer" target="_blank">
+            <GithubIcon fontSize="small" /> 
+            </Link>
+          </IconButton>
+          <IconButton> 
+            <Link color="inherit" href="https://www.linkedin.com/in/anthony-magana/" rel="noopener noreferrer" target="_blank">
+            <LinkedInIcon fontSize="small" /> 
+            </Link>
+          </IconButton>
+          <IconButton> 
+            <Link color="inherit" href="mailto:anthonymagana0705@gmail.com'" rel="noopener noreferrer" target="_blank">
+            <EmailIcon fontSize="small" /> 
+            </Link>
+          </IconButton>
+          <div style={{paddingTop: '5px', paddingBottom: '20px'}}>
+            <Typography variant="caption" color="inherit">
+              © 2021 Anthony Magana
+            </Typography>
+          </div>
+        </div>
+      </Container>
     )
 }
